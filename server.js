8ae232b522d34a1e9327a7ffdb58cd708581ffb3
unknown <jmcc986@gmail.com>
2021-10-18 10:49:06 -0400

    10/18

--- server.js
+++ server.js
@@ -1,15 +1,36 @@
+// Import NPM Packages
 const express = require('express');
 const dotenv = require('dotenv');
 
+// Import Local Files
+const connectDB = require('./config/connectdb');
+
+// Load dotenv variables
 dotenv.config({ path: "./config/config.env"})
 
+// Express config
 const app = express();
 const PORT = process.env.PORT || 3000
 
+
+// connect to db
+connectDB()
+
+// Import Routes
 const decadesRoutes = require('./routes/decades')
 
-app.use('api/v1/decades', decadesRoutes)
+app.use('/api/v1/decades', decadesRoutes)
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Server listening in ${process.env.NODE_ENV} on port ${PORT}`)
+})
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    //  Log problem to console
+    console.log(`Unhandled Promise Rejection: ${err.message}`)
+    // Stop Server and the process
+    server.close(() => {
+        process.exit(1)
+    })
 })
